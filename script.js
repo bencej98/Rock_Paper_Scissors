807cdf02a7297c0abbd8bd333a918e316929b9bb
Bence Jenei <bencej9812@gmail.com>
2023-01-25 14:45:31 +0100

    Create playRound function

--- script.js
+++ script.js
@@ -23,18 +23,25 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "rock" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "scissors") {
         alert("It's a tie!");
+        return "tie";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         alert("You lose! Paper beats Rock");
+        return "loose";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         alert("You win! Rock beats Scissors");
+        return "win";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         alert("You win! Paper beats Rock");
+        return "win";
     } else if (playerSelection === "paper" && computerSelection === "scissors")  {
         alert("You lose! Scissors beats Paper")
+        return "loose";
     } else if(playerSelection === "scissors" && computerSelection === "rock") {
         alert("You lose! Rock beats Scissors")
+        return "loose";
     } else if(playerSelection === "scissors" && computerSelection === "paper") {
         alert("You win! Scissors beats Paper")
+        return "win";
     }
 }
 
